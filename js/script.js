8831fe8185a6d4179ae5f8a05fb2c6ee281bcc7c
jakub-malhotra <124389450+jakub-malhotra@users.noreply.github.com>
2023-05-01 17:25:28 +0000

    initial commit

--- js/script.js
+++ js/script.js
@@ -1,5 +1,31 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2020 Mr.Coxall All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Jakub Malhotra
+// Created on: May 2023
 // This file contains the JS functions for index.html
+
+"use strict"
+
+/**
+
+/**
+ * This function updates the slider value.
+ */
+function myButtonClicked() {
+  document.getElementById("age").innerHTML = userAge
+
+  if (userAge >= 17) {
+    document.getElementById("movie-rating").innerHTML =
+      "You can see any movie."
+  } else if (userAge >= 13) {
+    document.getElementById("movie-rating").innerHTML =
+      "You can see a PG 13 rated movie."
+  } else if (userAge >= 5) {
+    document.getElementById("movie-rating").innerHTML =
+      "You can see up to a PG or G rated movie."
+  } else {
+    document.getElementById("movie-rating").innerHTML =
+      "You are too young for most movies."
+  }
+}
+
