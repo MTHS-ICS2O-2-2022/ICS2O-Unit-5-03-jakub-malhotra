440ee23eef4cb445a0631c101f0bea6a812857bc
jakub-malhotra <124389450+jakub-malhotra@users.noreply.github.com>
2023-05-01 22:35:11 +0000

    commit

--- js/script.js
+++ js/script.js
@@ -4,28 +4,26 @@
 // Created on: May 2023
 // This file contains the JS functions for index.html
 
-"use strict"
-
-/**
+"use strict";
 
 /**
  * This function updates the slider value.
  */
 function myButtonClicked() {
-  document.getElementById("age").innerHTML = userAge
+  const userAge = document.getElementById("age").value;
+  document.getElementById("age").innerHTML = userAge;
 
   if (userAge >= 17) {
     document.getElementById("movie-rating").innerHTML =
-      "You can see any movie."
+      "You can see any movie.";
   } else if (userAge >= 13) {
     document.getElementById("movie-rating").innerHTML =
-      "You can see a PG 13 rated movie."
+      "You can see a PG 13 rated movie.";
   } else if (userAge >= 5) {
     document.getElementById("movie-rating").innerHTML =
-      "You can see up to a PG or G rated movie."
+      "You can see up to a PG or G rated movie.";
   } else {
     document.getElementById("movie-rating").innerHTML =
-      "You are too young for most movies."
+      "You are too young for most movies.";
   }
 }
-
